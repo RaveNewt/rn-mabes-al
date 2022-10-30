af1b1bd0930b1a98e023a2266c29a3d574e0a8e8
RaveNewt <ihsanulisra@gmail.com>
2022-10-30 10:20:06 +0700

    first commit

---
+++ src/screens/Result/index.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Heading, HStack, Image, Stack } from "native-base";
+import { btn_add } from "../../assets";
+
+const Result = () => {
+    return (
+        <Stack>
+            <HStack p={4} shadow={2} justifyContent="center">
+                <Heading
+                    fontSize="2xl"
+                    alignItems={"center"}
+                    textAlign="center"
+                    flex={1}
+                >
+                    Cart App
+                </Heading>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("NewTodo")}
+                >
+                    <Image source={btn_add} size={8} borderColor="black" />
+                </TouchableOpacity>
+            </HStack>
+        </Stack>
+    );
+};
+
+export default Result;
+
+const styles = StyleSheet.create({});
